66076b680828346c03b213728d722d77df48f713
Abhishek Sikarwar <mail.abhisheksikarwar@gmail.com>
2025-02-16 21:39:30 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -11,36 +11,38 @@ const App = () => {
     useEffect(() => {
         dispatch(created());
     }, []);
-    if (isLoading == "loading") return <h4>Loading....</h4>;
     return (
         <>
             <h1>A short Naration of Lorem Ipsum</h1>
-            <div
-                style={{
-                    display: "grid",
-                    gridTemplateColumns: "repeat(2,1fr)",
-                    gap: "1rem",
-                }}
-            >
-                <ul>
-                    {randomData &&
-                        randomData.map((item, idx) => {
-                            return (
-                                <li
-                                    style={{ border: "1px solid black" }}
-                                    key={idx}
-                                >
-                                    <p>
-                                        <b>Title: </b> {item.title}
-                                    </p>
-                                    <p>
-                                        <b>Body: </b> {item.content}
-                                    </p>
-                                </li>
-                            );
-                        })}
-                </ul>
-            </div>
+            {isLoading === "loading" && <h4>Loading...</h4>}
+            {isLoading === "idle" && (
+                <div
+                    style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(2,1fr)",
+                        gap: "1rem",
+                    }}
+                >
+                    <ul>
+                        {randomData &&
+                            randomData.map((item, idx) => {
+                                return (
+                                    <li
+                                        style={{ border: "1px solid black" }}
+                                        key={idx}
+                                    >
+                                        <p>
+                                            <b>Title: </b> {item.title}
+                                        </p>
+                                        <p>
+                                            <b>Body: </b> {item.content}
+                                        </p>
+                                    </li>
+                                );
+                            })}
+                    </ul>
+                </div>
+            )}
         </>
     );
 };
