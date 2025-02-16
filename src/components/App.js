9ee59dd8289c21848f38721d5e3717e96d550984
Abhishek Sikarwar <mail.abhisheksikarwar@gmail.com>
2025-02-16 22:37:41 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -40,7 +40,10 @@ const App = () => {
                                             excepturi optio reprehenderit
                                         </p>
                                         <p className="body">
-                                            <b>Body :</b>quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto
+                                            <b>Body :</b>
+                                            {
+                                                "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+                                            }
                                         </p>
                                     </li>
                                 );
